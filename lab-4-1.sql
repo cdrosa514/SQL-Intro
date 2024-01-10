ee67a6f668a43d6ab8ca286535b33a60e1a4262d
Christian De Rosa <christian.derosa@kellogg.northwestern.edu>
2024-01-10 17:30:32 +0000

    SQL Lecture 2

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -3,4 +3,13 @@
 -- Expected result:
 -- 2935
 
+--SELECT players.id, players.first_name, players.last_name, hits
+--FROM stats INNER JOIN  on players.id = stats.id
+--WHERE players.id = 1678
+--GROUP BY year;
 
+SELECT SUM(stats.hits)
+FROM stats
+INNER JOIN players on stats.player_id = players.id
+WHERE first_name = "Barry"
+AND last_name = "Bonds"
